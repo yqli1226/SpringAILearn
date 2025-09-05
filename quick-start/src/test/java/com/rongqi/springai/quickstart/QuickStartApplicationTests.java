43e536a2fff46135ee019df9386e4ad0f014c83e
liyongqi <2297940456@qq.com>
2025-09-05 12:37:18 +0800

    添加子模块 删除子模块多余配置

---
+++ quick-start/src/test/java/com/rongqi/springai/quickstart/QuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rongqi.springai.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
