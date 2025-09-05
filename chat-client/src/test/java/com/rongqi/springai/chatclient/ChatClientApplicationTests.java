555f23386ecf8d6e7dae70c12b9f10183e5e94a3
liyongqi <2297940456@qq.com>
2025-09-05 15:47:20 +0800

    feat add chat-client module

---
+++ chat-client/src/test/java/com/rongqi/springai/chatclient/ChatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rongqi.springai.chatclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
