4c2a8b58311f98da53d6c80933862fbd668c22c6
liyongqi <2297940456@qq.com>
2025-09-13 20:22:48 +0800

    feat(mcp-stdio-server) 新增天气服务MCP服务器模块

---
+++ mcp-stdio-server/src/test/java/com/rongqi/springai/mcpstdioserver/McpStdioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rongqi.springai.mcpstdioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpStdioServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
